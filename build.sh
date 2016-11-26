0ec8c34976bf2cf5a9b444cd7177a9202a10b723
Ron Kurr <kurr@kurron.org>
2016-11-25 22:36:26 -0500

    Install Guest Additions as one of the final steps so video driver works properly

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ export PACKER_LOG=activate
 
 packer validate packer.json
 packer build --force packer.json
-vagrant box add --clean --force --name xenial-xubuntu/5.1.10 vagrant/xenial-xubuntu.box
+vagrant box add --clean --force --name xenial-xubuntu vagrant/xenial-xubuntu.box
 vagrant box list
