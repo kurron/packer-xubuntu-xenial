122f7e2b30f0126237db9ee25735f456fc7667a6
Ron Kurr <kurr@kurron.org>
2017-07-30 13:08:22 -0400

    Clean out files before zeroing disk

--- scripts/zero-out-disk.sh
+++ scripts/zero-out-disk.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 echo 'Clearing APT cache...'
-apt-get cleanapt-get clean
+apt-get clean
+
+echo 'Clearing out ~/Downloads directory...'
+rm -rf /home/vagrant/Downloads/*.zip
+rm -rf /home/vagrant/Downloads/*.gz
+rm -rf /home/vagrant/Downloads/*.sh
 
 echo 'Zeroing device to make space...'
 dd if=/dev/zero of=/EMPTY bs=1M
