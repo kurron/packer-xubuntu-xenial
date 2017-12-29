4d6064342084f09bf0110d5bd062f7f22edf9654
Ron Kurr <kurr@kurron.org>
2017-12-29 15:39:44 -0500

    Upgrade to VirtualBox 5.2.4

--- build.sh
+++ build.sh
@@ -5,6 +5,8 @@ export PACKER_LOG=activate
 
 PACKER_FILE=${1:-packer.json}
 
+#sleep 5h
+
 packer validate ${PACKER_FILE}
 packer build --force ${PACKER_FILE}
 vagrant box add --clean --force --name xenial-xubuntu vagrant/xenial-xubuntu.box
